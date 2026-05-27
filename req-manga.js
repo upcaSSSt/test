784d4c8a15e3df4bb5ceb6573d2c47b355ae23b2
harpCS <100532126+upcaSSSt@users.noreply.github.com>
2026-05-27 22:58:47 +0300

    Add files via upload

---
+++ req-manga.js
@@ -0,0 +1,20 @@
+import ImageKit from 'imagekit';
+import { writeFileSync } from 'node:fs';
+
+const imagekit = new ImageKit({
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+  urlEndpoint: process.env.URL_ENDPOINT,
+});
+
+const files = await imagekit.listFiles({ path: '/manga', limit: 1000 });
+console.log(files);
+/*const cleanFiles = files
+  .filter(file => file.fileType === 'image' && file.name.endsWith('.webp'))
+  .map(file => ({
+    name: file.name,
+    filePath: file.filePath,
+    url: file.url
+  }));*/
+
+writeFileSync('manga.json', JSON.stringify(files, null, 2));
